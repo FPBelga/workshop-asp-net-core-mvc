77317a69e0880472e3402c3a5bd2e8d23e1eb84a
FPBelga <belgasystem@gmail.com>
2024-05-12 10:39:39 -0300

    Delete Seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,32 +1,36 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
     public class SellerService
     {
         private readonly SalesWebMvcContext _context;
-
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
         }
-
         public List<Seller> Findall()
         {
             return _context.Seller.ToList();
         }
-
         public void Insert(Seller obj)
         {
             //obj.Department = _context.Department.First(); 
             _context.Add(obj);
             _context.SaveChanges();
-
+        }
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
         }
     }
 }
